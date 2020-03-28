2c2fea85bc8609f16ecaa4bb0276f58f5dd27514
MarcelHuesmann <marcel-privat@mail.ru>
2020-03-28 11:20:48 +0000

    Added Procfile for deployment

--- run.py
+++ run.py
@@ -1,10 +1,43 @@
 import os
-from flask import Flask
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session, url_for
+
 
 app = Flask(__name__)
+app.secret_key =  os.hetenv("SECRET" , "randomstring123")
+messages = []
 
-@app.route('/')
+
+def add_message(username, message):
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append({"timestamp": now, "from": username, "message": message})
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "<h1> Hello There! </h1>"
+    """Main page with instructions"""
+    if request.method == "POST":
+        session["username"] = request.form["username"]
 
-    app.run(os.getenv('IP'), port=init(os.getenv('PORT')), denu=True)
+    if "username" in session:
+        return redirect(url_for("user", username=session["username"]))
+
+    return render_template("index.html")
+
+
+@app.route("/chat/<username>", methods=["GET", "POST"])
+def user(username):
+    """Add and display chat messages"""
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_message(username, message)
+        return redirect(url_for("user", username=session["username"]))
+
+    return render_template("chat.html", username=username,
+                           chat_messages=messages)
+
+
+app.run(host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", "5000")), debug=False)
